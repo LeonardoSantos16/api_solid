f0f107d5176405b18ec6afda6496960d52f245b3
leonardo.mendes <leodsmf@gmail.com>
2024-11-09 19:04:20 -0300

    testes unitários e In memory database

---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vitest/config' // configuração para o vitest entender as importações com o alias @
+import tsconfigPaths from 'vite-tsconfig-paths'
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+})
