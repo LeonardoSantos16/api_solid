34c20999d66c082ad3429b236825d4d8c0a5badb
leonardo.mendes <leodsmf@gmail.com>
2024-12-07 11:27:18 -0300

    finalização do factory, repositorios, Test environment e controller de perfil

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from 'vitest/config' // configuração para o vitest ent
 import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    dir: 'src',
+  },
 })
